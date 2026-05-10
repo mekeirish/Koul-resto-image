143cfa4373f0eaf00bc18ac02008e91347e5587c
mekeirish <mmm1747lb@gmail.com>
2026-05-10 21:48:26 +0200

    Add manifest.js for web app configuration

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+    "short_name": "App",
+    "name": "App",
+    "icons": [
+        {
+            "src": "icon.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+        }
+    ],
+    "start_url": "./index.html",
+    "display": "standalone",
+    "theme_color": "#4a90e2",
+    "background_color": "#4a90e2"
+}
